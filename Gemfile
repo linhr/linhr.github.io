c73ac7b1f9e754a2020c31ec48c64d9f89b930ff
linhr <linhr10@gmail.com>
2013-10-30 21:54:22 +0800

    add stylesheets

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'github-pages'
+gem 'compass', '>= 0.12.2'
+gem 'susy'
---
+++ config.rb
@@ -0,0 +1,12 @@
+# Compass configuration
+
+require 'susy'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# output style: expanded / nested / compact / compressed
+output_style = :compressed
